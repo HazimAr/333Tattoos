41f029c28b950f03142b825b221e96ae2c2890ea
Hazim Arafa <hazimarafa80@gmail.com>
2021-06-27 13:16:00 -0700

    Fixed Devil At Footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,59 +1,44 @@
-import {
-	Box,
-	Flex,
-	Heading,
-	Text,
-	Link,
-	Image,
-	Circle,
-} from "@chakra-ui/react";
-import { FaTwitter, FaInstagram } from "react-icons/fa";
+import { Box, Flex, Heading, Icon, Image, Link, Text } from "@chakra-ui/react";
+import { FaInstagram, FaTwitter } from "react-icons/fa";
 
 // eslint-disable-next-line import/no-default-export
 export default function footer(): JSX.Element {
 	return (
-		<Box as="footer" mt="100vh">
-			<Heading size="md" color="text.100">
-				Interested in my work?
-			</Heading>
-			<Text my="15px" color="text.400">
-				I'm currently taking on new projects. Would you like to discuss
-				yours?
-			</Text>
-			<Box color="text.500">
-				<Link href="tel:8592406617" isExternal>
-					+1 859-240-6617
-				</Link>
-				<br />
-				<Link href="mailto:devil@333tattoos.art" isExternal>
-					devil@333tattoos.art
-				</Link>
-			</Box>
-			<Flex justify="center" align="center" mt="20px">
-				<Flex justify="space-between" w="100%" maxW="50px">
-					<Link
-						as={Circle}
-						bg="red"
-						p="2.5px"
-						href="https://twitter.com/333tattoos"
-						isExternal
-					>
-						<FaTwitter size="20px" />
+		<Flex as="footer" mt="100vh" justify="center" align="center">
+			<Box>
+				<Heading size="md" color="text.100">
+					Interested in my work?
+				</Heading>
+				<Text my="15px" color="text.400">
+					I'm currently taking on new projects. Would you like to
+					discuss yours?
+				</Text>
+				<Box color="text.500">
+					<Link href="tel:8592406617" isExternal>
+						+1 859-240-6617
 					</Link>
-					<Link
-						as={Circle}
-						bg="red"
-						p="2.5px"
-						href="https://www.instagram.com/333.tattoos"
-						isExternal
-					>
-						<FaInstagram size="20px" />
+					<br />
+					<Link href="mailto:devil@333tattoos.art" isExternal>
+						devil@333tattoos.art
 					</Link>
+				</Box>
+				<Flex justify="center" align="center" mt="20px">
+					<Flex justify="space-between" w="100%" maxW="50px">
+						<Link href="https://twitter.com/333tattoos" isExternal>
+							<Icon as={FaTwitter} size="20px" />
+						</Link>
+						<Link
+							href="https://www.instagram.com/333.tattoos"
+							isExternal
+						>
+							<Icon as={FaInstagram} size="20px" />
+						</Link>
+					</Flex>
 				</Flex>
-			</Flex>
-			<Flex justify="center">
-				<Image src="/drawing/footer.png" w="500px" />
-			</Flex>
-		</Box>
+				<Flex justify="center">
+					<Image src="/drawing/devil2.png" w="200px" />
+				</Flex>
+			</Box>
+		</Flex>
 	);
 }
