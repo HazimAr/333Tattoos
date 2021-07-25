61d38759ac5c3a24a8bb7d44b9a3f95dc09f31ce
Hazim Arafa <hazimarafa80@gmail.com>
2021-07-25 14:13:16 -0700

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,15 +1,17 @@
 import {
 	Box,
-	Flex,
 	Heading,
+	HStack,
 	Icon,
 	Image,
 	Link,
 	Text,
 	useToken,
+	VStack,
 } from "@chakra-ui/react";
 import Container from "@components/container";
 import ContainerInside from "@components/containerInside";
+import React from "react";
 import { FaInstagram, FaTwitter } from "react-icons/fa";
 import Wave from "react-wavify";
 
@@ -31,51 +33,57 @@ export default function footer(): JSX.Element {
 				/>
 			</Box>
 			<Container bg="brand.secondary">
-				<ContainerInside as="footer" justify="center" align="center">
-					<Heading as="h1">Interested in my work?</Heading>
-					<Heading
-						my="15px"
-						color="text.400"
-						fontSize="lg"
-						as="h2"
-						fontWeight="normal"
-					>
-						I'm currently taking on new projects. Would you like to
-						discuss yours?
-					</Heading>
-					<Box color="text.500">
-						<Link href="tel:8592406617" isExternal>
-							<Text fontSize="lg">+1 859-240-6617</Text>
-						</Link>
+				<ContainerInside as="footer">
+					<VStack>
+						<VStack spacing={5}>
+							<Heading as="h1">Interested in my work?</Heading>
+							<Heading
+								color="text.400"
+								fontSize="lg"
+								as="h2"
+								fontWeight="normal"
+							>
+								I'm currently taking on new projects. Would you
+								like to discuss yours?
+							</Heading>
+							<VStack color="text.500" spacing={0}>
+								<Link href="tel:8592406617" isExternal>
+									<Text fontSize="lg">+1 859-240-6617</Text>
+								</Link>
 
-						<Link href="mailto:devil@333tattoos.art" isExternal>
-							<Text fontSize="lg">devil@333tattoos.art</Text>
-						</Link>
-					</Box>
-					<Flex justify="center" align="center" mt="20px">
-						<Flex justify="space-between" w="100%" maxW="50px">
-							<Link
-								href="https://twitter.com/333tattoos"
-								isExternal
-							>
-								<Icon as={FaTwitter} />
-							</Link>
-							<Link
-								href="https://www.instagram.com/333.tattoos"
-								isExternal
-							>
-								<Icon as={FaInstagram} />
-							</Link>
-						</Flex>
-					</Flex>
-					<Flex justify="center">
+								<Link
+									href="mailto:devil@333tattoos.art"
+									isExternal
+								>
+									<Text fontSize="lg">
+										devil@333tattoos.art
+									</Text>
+								</Link>
+							</VStack>
+
+							<HStack>
+								<Link
+									href="https://twitter.com/333tattoos"
+									isExternal
+								>
+									<Icon as={FaTwitter} boxSize="30px" />
+								</Link>
+								<Link
+									href="https://www.instagram.com/333.tattoos"
+									isExternal
+								>
+									<Icon as={FaInstagram} boxSize="30px" />
+								</Link>
+							</HStack>
+						</VStack>
+
 						<Image
 							src="/drawing/devil2.png"
 							w="200px"
 							h="260px"
 							alt="devilish girl holding a red trident"
 						/>
-					</Flex>
+					</VStack>
 				</ContainerInside>
 			</Container>
 		</>
